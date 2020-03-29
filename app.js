5415a74fe4cab7f7e5393d4335e117fc7ed4482e
Michał Pisula <62832101+mpisula94@users.noreply.github.com>
2020-03-29 16:31:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// var functions = require("./logic/functions");
+
+// functions.sayHello()
+// console.log(functions.sum(3, 2));
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+const newVaribles = require("./js/func");
+
+app.set("view engine", 'hbs');
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+app.use("/js", express.static(path.join(__dirname, "./js")));
+const sample = () => {
+    return "proba"
+}
+
+app.get("/", function(req, res) {
+    // res.send("Hello World")
+    res.render("index", {
+        pageTitle: "lekcja",
+        sampleText: newVaribles.someTitle,
+        thirdTitle: sample()
+
+    })
+})
+
+app.get("/about", function(req, res) {
+    res.send("my site")
+})
+
+app.listen(port, (err) => {
+    if (err) { return console.log("Error: ", err) }
+    console.log("serwer działa na porcie " + port);
+});
---
+++ js/func.js
@@ -0,0 +1,15 @@
+//alert("Hello")
+
+//const example = () => {
+//    const fromInput = document.getElementById("example").value;
+//    alert(fromInput);
+//    document.getElementsByTagName("p")[0].innerText = fromInput;
+//}
+
+//document.getElementById("action").addEventListener("click", example)
+
+const someTitle = "New example"
+module.exports = {
+
+    someTitle
+}
